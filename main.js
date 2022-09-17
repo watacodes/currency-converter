7373a0a09a0ef91462bf74e0e413ce2d32de5176
Wataru Hori <Bireleysaddiction@gmail.com>
2022-09-17 16:28:53 +0900

    Created a dropdown button and get currencies via an API

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
 const convertButton = document.querySelector('.convert-btn');
 const placeholder = document.querySelector('.placeholder');
-convertButton.addEventListener('click', apiInit);
 
-function apiInit() {
+let currency;
+
+convertButton.addEventListener('click', getAPI);
+
+function appInit() {
+  fetch(`https://api.vatcomply.com/rates`)
+    .then(res => res.json())
+    .then(data => {
+      currency = Object.keys(data.rates)
+      console.log(currency);
+      loadCurrencyList();
+      })
+    .catch(err => console.log(err));
+}
+
+appInit();
+
+function loadCurrencyList() {
+  const originalCurrency = document.getElementById('from');
+  const convertedCurrency = document.getElementById('to');
+  for (let i = 0; i < currency.length; i++) {
+    console.log(originalCurrency[originalCurrency.length]);
+    originalCurrency[i] = new Option(currency[i]);
+    convertedCurrency[i] = new Option(currency[i]);
+  }
+}
+
+function getAPI() {
   const getCurrency = document.getElementById('from').value.toUpperCase().trim();
   const convertTo = document.getElementById('to').value.toUpperCase().trim();
   fetch(`https://api.vatcomply.com/rates?base=${encodeURIComponent(getCurrency)}`)
@@ -13,3 +39,4 @@ function apiInit() {
     })
     .catch(err => console.log(err));
 }
+
